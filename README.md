70b1a5560897b02325b9321df8a2f5fe867b3f29
Brody Childs <maximombro@gmail.com>
2023-12-21 22:06:38 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# MBM's Music Visualizer
+
+>
+> Nothing fuzzy about it.
+>
+
+## What is it?
+
+A music visualizer.
---
+++ __init__.py
@@ -0,0 +1,18 @@
+# MBM's Music Visualizer
+
+# Imports
+# etc...
+
+# A dictionary that contains all nodes you want to export with their names
+# NOTE: names should be globally unique
+NODE_CLASS_MAPPINGS = {
+    # "id": Class
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    # "id": "readable"
+}
+
+# Export
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
