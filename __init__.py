e1d8060022af10aac13b01f80f9a1662664cc4c5
Brody Childs <maximombro@gmail.com>
2024-01-29 22:13:32 -0500

    Added Prompt Sequence nodes

--- __init__.py
+++ __init__.py
@@ -3,20 +3,23 @@
 # Imports
 from .mbmMusicVisualizer import MusicVisualizer
 from .mbmAudioLoader import AudioLoader
+from .mbmPromptSequence import PromptSequenceBuilder
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
     # "id": Class
     "mbmMusicVisuzalizer": MusicVisualizer,
-    "mbmAudioLoader": AudioLoader
+    "mbmAudioLoader": AudioLoader,
+    "mbmPromptSequenceBuilder": PromptSequenceBuilder
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     # "id": "readable"
     "mbmMusicVisuzalizer": "Music Visualizer",
-    "mbmAudioLoader": "Audio Loader"
+    "mbmAudioLoader": "Audio Loader",
+    "mbmPromptSequenceBuilder": "Prompt Sequence Builder"
 }
 
 # Export
---
+++ mbmPromptSequence.py
@@ -0,0 +1,53 @@
+# MBM's Music Visualizer: Prompt Sequence
+# Allows for building of a sequence of prompts.
+
+# Imports
+import torch
+
+# Classes
+class PromptSequenceBuilder:
+    """
+    Allows for building of a sequence of prompts.
+    """
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "positive_1": ("CONDITIONING", ),
+                "negative_1": ("CONDITIONING", )
+            },
+            "optional": {
+                "positive_2": ("CONDITIONING", ),
+                "negative_2": ("CONDITIONING", ),
+                "prompts": ("PROMPT_SEQ", )
+            }
+        }
+
+    RETURN_TYPES = ("PROMPT_SEQ", )
+    RETURN_NAMES = ("PROMPTS", )
+    FUNCTION = "process"
+    CATEGORY = "MBMnodes/Prompts"
+
+    def process(self, positive_1, negative_1, positive_2 = None, negative_2 = None, prompts: torch.Tensor = None):
+        """
+        Returns a Tensor of the shape `[num of prompt sets, 2, *input tensor shape]`.
+        """
+        # Create a new prompt sequence
+        if (positive_2 is None) or (negative_2 is None):
+            # Include just the required one
+            promptsOut = torch.stack((positive_1[0][0].squeeze(), negative_1[0][0].squeeze())).unsqueeze(0)
+        else:
+            # Include both
+            promptsOut = torch.stack((
+                torch.stack((positive_1[0][0].squeeze(), negative_1[0][0].squeeze())),
+                torch.stack((positive_2[0][0].squeeze(), negative_2[0][0].squeeze()))
+            ))
+
+        # Add to the given prompt sequence if provided
+        if prompts is not None:
+            promptsOut = torch.cat((prompts, promptsOut), dim=0)
+
+        return (promptsOut, )
